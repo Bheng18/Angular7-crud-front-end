f62baad7bab7c2792a7a6ac6534d35265e65f2ef
Bheng18 <joebert.fundador@gmail.com>
2019-06-16 20:47:15 -0800

    make it better

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 //install express server
 const express = require('expresss');
+const http = require('http');
 const path = require('path');
 
 const app = express();
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8082;
+app.set('port', port);
 
 //serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/angular-crud-app'));
 
 app.get('*', (req, res) => {
-   res.sendFile(path.join(__dirname + '/dist/index.html'));
+   res.sendFile(path.join(__dirname + '/dist/angular-crud-app/index.html'));
 });
 
-app.listen(port, console.log('running in port 8080'));
+const server = http.createServer(app);
+server.listen(port, console.log('running in port 8080'));
