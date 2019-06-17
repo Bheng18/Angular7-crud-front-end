98d31998e07d970708b7c033c6b358b89ada6c22
Bheng18 <joebert.fundador@gmail.com>
2019-06-16 21:47:55 -0800

    debug error line 6 server.js about express

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const path = require('path');
 
 const app = express();
 
-const port = process.env.PORT || 8082;
+const port = process.env.PORT || '8082';
 app.set('port', port);
 
 //serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
-app.use(express.static(__dirname + '/dist/angular-crud-app'));
+app.use(express.static(path.join(__dirname + '/dist')));
+app.use(express.static(path.join(__dirname + '/dist/angular-crud-app')));
 
 app.get('*', (req, res) => {
    res.sendFile(path.join(__dirname + '/dist/angular-crud-app/index.html'));
